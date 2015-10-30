4ef774eb0d663f2a36fd033641cb40b41f49ddf7
Tim Lampman <timothy.lampman@gmail.com>
2015-10-29 22:37:33 -0400

    Added capability to handle battery state of charge (battery_soc) events and issue MySQL command

--- Thermal Network Server/ServerProgram.cs
+++ Thermal Network Server/ServerProgram.cs
@@ -31,8 +31,8 @@ namespace ThermalNetworkServer {
 		private const int SERVER_PORT = 6232;				// The listening port of the source commands
 
 		// Database
-		private const string DB_INSERT = "GET /db_test_upload.php?";	// DEBUGGING - The html command to upload the data into the database
-//		private const string DB_INSERT = "GET /db_sensor_upload.php?";	// The html command to upload the data into the database
+		private const string DB_INSERT = "GET /db_sensor_upload.php?";	// The html command to upload the data into the database
+//		private const string DB_INSERT = "GET /db_test_upload.php?";	// DEBUGGING - The html command to upload the data into the database
 
 		//=====================================================================
 		// XBEE SETUP
@@ -50,6 +50,7 @@ namespace ThermalNetworkServer {
 		const byte HEATING_CODE		=  7;
 		const byte THERMOSTAT_CODE	=  8;
 		const byte TEMP_12BYTE_CODE	=  9;
+		const byte BATTERY_SOC_CODE	= 10;
 
 		// XBee command codes
 		const byte CMD_THERMO_POWER = 1;
@@ -78,8 +79,10 @@ namespace ThermalNetworkServer {
 		// NETWORKING MEMBERS/CONSTANTS
 		//=====================================================================
 		// Addresses
-		private const string RELAY_ADDRESS = "00 13 A2 00 40 AE B9 7F";	// The address of the xbee radio controlling the relay
-		private const string CONTROL_ADDRESS = "40aeba93";	// The short address of this controller radio (attached to this netduino plus) for DB identification
+		private const string RELAY_ADDRESS = "00 13 A2 00 40 AB 97 78";	// The address of the xbee radio controlling the relay
+//		private const string RELAY_ADDRESS = "00 13 A2 00 40 AE B9 7F";	// DEBUGGING - The address of the xbee radio controlling the relay
+		private const string CONTROL_ADDRESS = "4060ad63";	// The short address of this controller radio (attached to this netduino plus) for DB identification
+//		private const string CONTROL_ADDRESS = "40aeba93";	// DEBUGGING - The short address of this controller radio (attached to this netduino plus) for DB identification
 		private const string XBEE_PORT = "COM1";
 
 		// Messaging
@@ -95,8 +98,8 @@ namespace ThermalNetworkServer {
 		private static TMP102BusSensor tempSensor = new TMP102BusSensor();
 
 		// Timing
-//		private const int SENSOR_DELAY = 600000;	// The delay in microseconds between sensor readings
-		private const int SENSOR_DELAY = 60000;	// DEBUGGING - The delay in microseconds between sensor readings
+		private const int SENSOR_DELAY = 600000;	// The delay in microseconds between sensor readings
+//		private const int SENSOR_DELAY = 60000;	// DEBUGGING - The delay in microseconds between sensor readings
 
 		//=====================================================================
 		// MAIN PROGRAM
@@ -142,13 +145,13 @@ namespace ThermalNetworkServer {
 			// Infinite loop to collect local sensor data
 			//-----------------------------------------------------------------
 			while(true) {
-				// Get the sensor reading
+/*				// Get the sensor reading
 				double temperature = tempSensor.readTemperature();
 
 				// Update the database - Air temperature
 				string sensorUpdate = DB_INSERT + "radio_id=" + CONTROL_ADDRESS + "&temperature=" + temperature.ToString("F2") + "&power=3.3\r\n";
 				SendNetworkRequest(sensorUpdate, IPAddress.Parse(DB_ADDRESS), DB_PORT);
-
+*/
 				// Sleep on this thread for the sensor period
 				Thread.Sleep(SENSOR_DELAY);
 			}
@@ -747,6 +750,7 @@ namespace ThermalNetworkServer {
 					else if(packetData[byte_pos] == LUX_CODE) dataUpdate += "&luminosity_lux=";
 					else if(packetData[byte_pos] == HEATING_CODE) dataUpdate += "&heating_on=";
 					else if(packetData[byte_pos] == THERMOSTAT_CODE) dataUpdate += "&thermo_on=";
+					else if(packetData[byte_pos] == BATTERY_SOC_CODE) dataUpdate += "&battery_soc=";
 					else return;	// Something funny happened
 					++byte_pos;
 
